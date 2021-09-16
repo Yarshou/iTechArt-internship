160a1965420570d3b13648693678a8c60c7d5fb7
Yarshou <dershov03@yandex.by>
2021-09-16 18:18:33 +0300

    fixed pep w391 issue + converted from float to decimal

--- task1.py
+++ task1.py
@@ -1,22 +1,11 @@
-import fractions
-import decimal
-import collections
-import typing
-
-
-def get_max_and_min(data: typing.Union[float, fractions.Fraction, str]) -> tuple:
+def get_max_and_min(data: typing.Set[typing.Union[fractions.Fraction, decimal.Decimal, str]]) -> tuple:
     data = list(data)
     for elem in enumerate(data):
         if isinstance(elem[1], str):
             try:
-                data[elem[0]] = float(data[elem[0]])
-            except ValueError:
+                data[elem[0]] = decimal.Decimal(data[elem[0]])
+            except decimal.InvalidOperation:
                 data[elem[0]] = fractions.Fraction(elem[1].replace(" ", "").replace("\\", "/"))
-    Point = collections.namedtuple('Point', ['min_value', 'max_value'])
-    p = Point(min_value=min(data), max_value=max(data))
+    Point = collections.namedtuple('Point', ['max_value', 'min_value'])
+    p = Point(max_value=max(data), min_value=min(data))
     return p
-
-
-print(get_max_and_min({5 / 124, '123.123', '4 \ 9'}))
-print(get_max_and_min({5 / 6, '123.123', '4 \ 9'}).min_value)
-print(get_max_and_min({5 / 6, '123.123', '4 \ 9'}).max_value)
