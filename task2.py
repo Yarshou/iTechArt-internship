fba9eb65df42e59da02688ea5b623e870394b0d5
Yarshou <dershov03@yandex.by>
2021-09-17 16:16:50 +0300

    changed from addition to multiplication of marks and added new logic

--- task2.py
+++ task2.py
@@ -1,34 +1,23 @@
-import typing
 import functools
-
-DATA = {
-    "Vasya": {
-        "Mathematics": [1, 6, 3, 8, 3, 4, 3],
-        "Russian": [1, 6, 3, 1, 3, 1, 3],
-        "Belarussian": [1, 6, 3, 8, 3, 1, 3],
-    },
-    "Petya": {
-        "Esd": [8, 9, 9, 9, 8, 9],
-        "Russian": [1, 6, 3, 8, 3, 1, 3],
-        "fC": [1, 6, 3, 8, 3, 1, 3],
-    }
-}
 
 
 def analyze_students(data: dict) -> set:
-    return set((x, y, functools.reduce(lambda el1, el2: el1 + el2, data[x][y])) for x in data.keys() for y in data[x] if y != "1C")
+    return set((x, y, functools.reduce(lambda el1, el2: el1 * el2, data[x][y])) for x in data.keys() for y in data[x] if
+               y != "1C")
 
 
 def validate_data(data: dict) -> bool:
     for name in data.keys():
         if not isinstance(name, str):
             raise TypeError
-        if not name.isascii() or any(symbol.isdigit() for symbol in name):
+        if not name.isascii() or not name.isalpha() or not name.isalnum():
             raise ValueError
         for subject in data[name]:
+            if subject == "1C":
+                continue
             if not isinstance(subject, str):
                 raise TypeError
-            if not subject.isascii() or any(symbol.isdigit() for symbol in subject):
+            if not subject.isascii() or not subject.isalpha() or not subject.isalnum():
                 raise ValueError
             for mark in data[name][subject]:
                 if isinstance(mark, bool):
@@ -38,7 +27,3 @@ def validate_data(data: dict) -> bool:
                 if mark > 10 or mark < 1:
                     raise ValueError
     return True
-
-
-print(analyze_students(DATA))
-print(validate_data(DATA))
